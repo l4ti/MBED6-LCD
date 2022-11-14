93376970e28c5dede5f5ff5f94d38272981ab26c
Renato Castro <102629460+l4ti@users.noreply.github.com>
2022-11-13 23:55:13 -0300

    4th rev
    Copyright change

--- TextLCD.h
+++ TextLCD.h
@@ -1,5 +1,5 @@
 /* mbed TextLCD Library, for a 4-bit LCD based on HD44780
- * Copyrigth (c) 2022, Renato de Castro Ferreira
+ * Copyrigth (c) 2022, Renato de Castro Ferreira (https://github.com/l4ti)
  * Copyright (c) 2007-2010, sford, http://mbed.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
